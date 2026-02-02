40dd1145455c6a7fbf053457d191dd8491ab69fa
Argenis Cuellar <argenis.cuellar01@gmail.com>
2026-02-01 20:59:00 -0600

    feat: twoSum

---
+++ examples/go.mod
@@ -0,0 +1,3 @@
+module argeniscuellar/examples
+
+go 1.25.1
---
+++ examples/twoSum.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func twoSum(nums []int, target int) []int {
+	numMap := make(map[int]int)
+
+	for i, num := range nums {
+		complement := target - num
+		if j, ok := numMap[complement]; ok {
+			return []int{j, i}
+		}
+		numMap[num] = i
+	}
+	return nil
+}
+
+func main() {
+	nums := []int{2, 11, 15, 7}
+	target := 9
+	fmt.Println(twoSum(nums, target))
+}
