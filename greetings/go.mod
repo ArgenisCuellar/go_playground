1fe7c38272c0ddf5b0d2041720c21e58d4f22099
Argenis Cuellar <argenis.cuellar01@gmail.com>
2026-02-01 20:46:14 -0600

    feat: hellos function

---
+++ greetings/go.mod
@@ -0,0 +1,3 @@
+module argeniscuellar/greetings
+
+go 1.25.1
--- main/main.go
+++ main/main.go
@@ -3,10 +3,19 @@ package main
 import (
 	"argeniscuellar/greetings"
 	"fmt"
+	"log"
 )
 
 func main() {
 	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
+	log.SetPrefix("main: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("Argenis")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
